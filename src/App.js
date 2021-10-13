336226eb78db16ada91e394880840f686aadd58c
Eduardo Moura Tavares <emtavares@protonmail.com>
2021-10-13 10:30:16 -0300

    sketch pad mock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const SketchPad = styled.div`
+  height: 640px;
+  width: 640px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const Asdf = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Pixel = styled.div`
+  height: 64px;
+  width: 64px;
+  background-color: red;
+`;
+
+const createGrid = (size) => {
+  return [...Array(size - 1)].map((_, index) => <Pixel key={index} />);
+};
 
 function App() {
+  //const arr = new Array(10);
   return (
-    <div className="App">
-      Etch A Sketch
-    </div>
+    <Asdf>
+      <SketchPad>
+        <Pixel />
+        {createGrid(100)}
+      </SketchPad>
+    </Asdf>
   );
 }
 
