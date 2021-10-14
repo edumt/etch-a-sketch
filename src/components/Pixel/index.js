6d488bd98cca069542bf7a4b6d7ab88b9ef76e6e
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-10-13 22:21:28 -0300

    added fallbacks values to pixels

--- src/components/Pixel/index.js
+++ src/components/Pixel/index.js
@@ -7,6 +7,7 @@ const Pixel = ({ size }) => {
   const randomColor = () =>
     "#" + Math.floor(Math.random() * 16777215).toString(16);
   const handleHover = () => {
+    //todo: track if left button is pressed
     console.log(MouseEvent.buttons);
     setColor(randomColor());
   };
--- src/components/Pixel/styled.js
+++ src/components/Pixel/styled.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const Pixel = styled.div.attrs((props) => ({
   style: {
-    height: props.size,
-    width: props.size,
-    backgroundColor: props.color,
-    borderWidth: props.size * 0.08,
+    height: props.size || 75,
+    width: props.size || 75,
+    backgroundColor: props.color || "white",
+    borderWidth: props.size * 0.08 || 6,
   },
 }))`
   box-sizing: border-box;
