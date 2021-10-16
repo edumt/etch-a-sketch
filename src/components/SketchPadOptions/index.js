015524a14fe62daee374f0ab9cb9086aac957bd2
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-10-15 21:47:20 -0300

    added buttons for pending functionalities

--- src/components/SketchPadOptions/index.js
+++ src/components/SketchPadOptions/index.js
@@ -1,8 +1,33 @@
 //todo: implement slider to change resolution, maybe one to change size too
-import { Center } from "@mantine/core";
+import { Center, Button, Slider } from "@mantine/core";
 
 const SketchPadOptions = () => {
-  return <Center>asdf</Center>;
+  return (
+    <Center style={{ flexDirection: "column", width: 250 }}>
+      <Button>Picked Color</Button>
+      <Button>Eraser</Button>
+      <Button>Rainbow Mode</Button>
+      <Button>Tint</Button>
+      <Button>Shade</Button>
+      <Button>Toggle Grid Lines</Button>
+      {/* <Slider
+        defaultValue={16}
+        min={4}
+        max={100}
+        step={1}
+        label={(value) => value}
+        labelAlwaysOn
+      /> */}
+      <Slider
+        style={{ width: "90%", height: 30 }}
+        marks={[
+          { value: 20, label: "20%" },
+          { value: 50, label: "50%" },
+          { value: 80, label: "80%" },
+        ]}
+      />
+    </Center>
+  );
 };
 
 export default SketchPadOptions;
