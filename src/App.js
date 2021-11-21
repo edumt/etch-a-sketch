459275b4e092fac410238e988d7d7d5efa90006d
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-11-21 20:50:26 -0300

    [bugfix] first toggle grid line not working

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   );
   const [gridResolution, setGridResolution] = useState(16);
   const [clearSketchPad, setClearSketchPad] = useState(false);
-  const [isShowingGrid, setIsShowingGrid] = useState(false);
+  const [isShowingGrid, setIsShowingGrid] = useState(true);
 
   return (
     <Wrapper>
