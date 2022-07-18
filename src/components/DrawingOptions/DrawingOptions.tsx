499bb56067953f09aadd03dffcc44d0135e6a212
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-07-17 21:30:27 -0300

    DrawingOptions refactor

--- src/components/DrawingOptions/DrawingOptions.tsx
+++ src/components/DrawingOptions/DrawingOptions.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { updatePickedColor } from "../../redux/reducers/boardSlice";
 
-import { Center, ColorInput, ButtonVariant } from "@mantine/core";
+import { Center, ColorInput } from "@mantine/core";
 import Option from "../Option/Option";
 
 const DrawingOptions = () => {
@@ -13,11 +13,13 @@ const DrawingOptions = () => {
   const handleColorPicking = (color: string) =>
     dispatch(updatePickedColor(color));
 
-  const optionsVariant: ButtonVariant[] = [];
-  for (let i = 0; i < 5; i++) {
-    if (i === activeOption) optionsVariant[i] = "filled";
-    else optionsVariant[i] = "outline";
-  }
+  const options = [
+    { label: "Picked Color", drawMode: colorInput },
+    { label: "Eraser", drawMode: "eraser" },
+    { label: "Rainbow Mode", drawMode: "rainbow" },
+    { label: "Shade", drawMode: "shade" },
+    { label: "Tint", drawMode: "tint" },
+  ];
 
   return (
     <Center
@@ -28,59 +30,25 @@ const DrawingOptions = () => {
       }}
     >
       <ColorInput
-        style={{ margin: "5px 0" }}
+        styles={{ root: { margin: "5px 0" } }}
         value={colorInput}
         format="rgba"
-        onChange={(event) => {
-          setColorInput(event);
+        onChange={(color) => {
+          setColorInput(color);
           if (activeOption === 0) handleColorPicking(colorInput);
         }}
       />
-      <Option
-        variant={optionsVariant[0]!}
-        handleClick={() => {
-          setActiveOption(0);
-          handleColorPicking(colorInput);
-        }}
-      >
-        Picked Color
-      </Option>
-      <Option
-        variant={optionsVariant[1]!}
-        handleClick={() => {
-          setActiveOption(1);
-          handleColorPicking("eraser");
-        }}
-      >
-        Eraser
-      </Option>
-      <Option
-        variant={optionsVariant[2]!}
-        handleClick={() => {
-          setActiveOption(2);
-          handleColorPicking("rainbow");
-        }}
-      >
-        Rainbow Mode
-      </Option>
-      <Option
-        variant={optionsVariant[4]!}
-        handleClick={() => {
-          setActiveOption(4);
-          handleColorPicking("tint");
-        }}
-      >
-        Tint
-      </Option>
-      <Option
-        variant={optionsVariant[3]!}
-        handleClick={() => {
-          setActiveOption(3);
-          handleColorPicking("shade");
-        }}
-      >
-        Shade
-      </Option>
+      {options.map((option, idx) => (
+        <Option
+          variant={idx === activeOption ? "filled" : "outline"}
+          handleClick={() => {
+            setActiveOption(idx);
+            handleColorPicking(option.drawMode);
+          }}
+        >
+          {option.label}
+        </Option>
+      ))}
     </Center>
   );
 };
